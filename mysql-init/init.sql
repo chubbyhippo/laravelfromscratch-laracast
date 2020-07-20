09b5479ef3d2ceff3e3a3877e38808b95b91cf61
ch <mkmsacc@outlook.com>
2020-07-20 13:54:59 +0700

    migrations 101

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -10,15 +10,4 @@ SET NAMES utf8mb4;
 CREATE DATABASE `laravel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `laravel`;
 
-DROP TABLE IF EXISTS `posts`;
-CREATE TABLE `posts` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-    `slug` varchar(255) NOT NULL,
-      `body` text NOT NULL,
-        PRIMARY KEY (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-        INSERT INTO `posts` (`id`, `slug`, `body`) VALUES
-        (1, 'my-first-post',    'This is my first blog post');
-
-        -- 2020-07-20 03:59:33'''')))))''''''
+-- 2020-07-20 03:59:33'''')))))''''''
