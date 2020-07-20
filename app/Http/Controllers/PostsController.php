6a47ec4c8fecbd4b721d52c2a768725c22092b97
ch <mkmsacc@outlook.com>
2020-07-20 13:33:32 +0700

    add Post model

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
 
-        //        dd($post);
+        $post = Post::where('slug', $slug)->firstOrFail();
 
         return view('post', [
             'post' => $post
