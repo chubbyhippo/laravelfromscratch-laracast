e0befee0ae2cdf9e099ca8b42b77a8a3e0bdb82d
ch <mkmsacc@outlook.com>
2020-07-20 15:43:03 +0700

    add layout pages

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
 Route::get('/posts/{post}', 'PostsController@show');
